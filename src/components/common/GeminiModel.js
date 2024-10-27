90e493bb9069fb7aa1b9caa63116f645acd4e8b6
Vivek Sharma <114747498+viveksharma004@users.noreply.github.com>
2024-10-27 13:10:35 +0530

    Update GeminiModel.js

--- src/components/common/GeminiModel.js
+++ src/components/common/GeminiModel.js
@@ -1,19 +1,16 @@
 import React, { useState } from "react";
 import HighlightText from "../core/HomePage/HighlightText";
 import {RingLoader} from "react-spinners";
-import {  RxCross1 } from "react-icons/rx";
+import { RxCross2 } from "react-icons/rx";
+function GeminiModel({geminiHandler}) {
 
 
-export default function GeminiModel({geminiHandler}) {
-
-
+  const [value, setValue] = useState("");
   const [response, setResponse] = useState("");
-  // const [response, setResponse] = useState("");
   const [loading, setLoading] = useState(false);
 
   async function submitHandlerButton() {
     try {
-      // console.log("values ",value);
       setLoading(true);
       const options = {
         method: "POST",
@@ -28,7 +25,7 @@ export default function GeminiModel({geminiHandler}) {
       // const response = await fetch("http://localhost:8000/gemini", options);
       const response = await fetch("https://chat-assistant-model.onrender.com/gemini", options);
       const data = await response.json();
-      // console.log(data.response);
+      // console.log(data);
       setResponse(data.response);
     } catch (error) {
       console.error(error);
@@ -41,17 +38,17 @@ export default function GeminiModel({geminiHandler}) {
   };
 
   return (
-    // <div className="flex justify-center items-center ">
-      <div className="flex flex-col justify-center items-center h-[400px] w-[300px]  border-2 border-richblack-400 rounded-lg ">
+      <div className="flex flex-col justify-center items-center h-[400px] w-[300px]  border-2 border-richblack-400 rounded-lg overflow-hidden ">
         <div className="p-2 flex flex-col justify-center items-center h-full w-full bg-richblack-700 rounded-sm ">
-       <div className="flex relative"><HighlightText text={"Ask Me Anything"}></HighlightText> <RxCross1 className="absolute -right-[55%] text-white" onClick={geminiHandler}/> </div>
+        <div className="relative"><HighlightText text={"Ask Me Anything"}></HighlightText><div onClick={geminiHandler} 
+        className="absolute left-[180px] top-[3px] text-white hover:text-richblack-200"><RxCross2/></div></div>
           <div className="min-h-[75%] max-h-[75%] w-full border bg-white border-richblack-50 border-1 overflow-auto rounded-md px-2 py-1">
             {loading? (<RingLoader
                   color="#7e9eda"
                   speedMultiplier={1}
                   size={65}
                   className="absolute top-[40%] left-[40%]"
-                />): (response && <p className="font-sans font-medium font-edu-sa">{response}</p>)}
+                />): (response && <p className="font-edu-sa font-sans font-medium">{response}</p>)}
           </div>
 
           <div className="flex flex-col gap-2 w-full">
@@ -70,8 +67,8 @@ export default function GeminiModel({geminiHandler}) {
                     focus:border-slate-400 hover:border-slate-300 shadow-sm 
                     focus:shadow" />
                 <button type="submit" name="submit" onClick={submitHandlerButton}
-                className="absolute right-1 top-1 rounded bg-yellow-25 py-1 px-2.5
-                 border border-transparent text-center text-sm text-richblack-800 transition-all 
+                className="absolute right-1 top-1 rounded bg-yellow-50 py-1 px-2.5
+                 border border-transparent text-center text-sm text-white transition-all 
                  shadow-sm hover:shadow focus:bg-slate-700 focus:shadow-none active:bg-slate-700
                   hover:bg-slate-700 active:shadow-none font-semibold " >
                   Submit
@@ -81,7 +78,7 @@ export default function GeminiModel({geminiHandler}) {
           </div>
         </div>
       </div>
-    // </div>
   );
 }
 
+export default GeminiModel;
